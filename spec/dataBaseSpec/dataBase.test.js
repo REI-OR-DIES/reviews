0fd73c288bb215aaf80a9b88453c2205fcec1576
Ryan Evanyo <ryan.evanyo@gmail.com>
2021-01-20 15:57:32 -0700

    Added Put test

--- spec/dataBaseSpec/dataBase.test.js
+++ spec/dataBaseSpec/dataBase.test.js
@@ -1,6 +1,7 @@
-const {MongoClient} = require('mongodb');
+const { MongoNetworkError } = require('mongodb');
+const { MongoClient } = require('mongodb');
 
-describe('insert', () => {
+describe('dataBase', () => {
   let connection;
   let db;
 
@@ -19,10 +20,24 @@ describe('insert', () => {
   it('should insert a doc into collection', async () => {
     const users = db.collection('users');
 
-    const mockUser = {_id: 'some-user-id', name: 'John'};
+    const mockUser = { _id: 'some-user-id', name: 'John' };
     await users.insertOne(mockUser);
 
-    const insertedUser = await users.findOne({_id: 'some-user-id'});
+    const insertedUser = await users.findOne({ _id: 'some-user-id' });
     expect(insertedUser).toEqual(mockUser);
+  });
+
+  it('should update a doc based off an id', async () => {
+    const users = db.collection('users');
+
+    const mockUser = { _id: '1234', name: 'Bob' };
+    await users.insertOne(mockUser);
+
+    const query = { _id: '1234' };
+    const newValues = { $set: { name: 'Ryan' } };
+
+    users.updateOne(query, newValues);
+    const updatedUser = await users.findOne({ _id: '1234' });
+    expect(updatedUser.name).toEqual('Ryan');
   });
 });
