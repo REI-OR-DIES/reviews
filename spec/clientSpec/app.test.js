46c9932051990cb4c44168414788c2a5dedff23a
Ryan Evanyo <ryan.evanyo@gmail.com>
2021-01-20 22:18:48 -0700

    Test

--- spec/clientSpec/app.test.js
+++ spec/clientSpec/app.test.js
@@ -3,7 +3,7 @@ import Enzyme, { shallow } from 'enzyme';
 import expect from 'expect';
 import Adapter from 'enzyme-adapter-react-16';
 import App from '../../client/src/components/App';
-import ReviewSnapShot from '../../client/src/components/ReviewSnapShot';
+// import ReviewSnapShot from '../../client/src/components/ReviewSnapShot';
 
 Enzyme.configure({ adapter: new Adapter() });
 
@@ -14,21 +14,6 @@ describe('App Component', () => {
   });
   it('Renders App Component', () => {
     const wrapper = shallow(<App />);
-    expect(wrapper.length).toEqual(1);
-  });
-});
-
-xdescribe('Review Snap Shot', () => {
-  it('Contains Review Snap Shot Class', () => {
-    const wrapper = shallow(<ReviewSnapShot />);
-    expect(wrapper.exists('.ReviewSnapShot')).toEqual(true);
-  });
-  it('Contains Action Box Class', () => {
-    const wrapper = shallow(<ReviewSnapShot />);
-    expect(wrapper.exists('.actionBox')).toEqual(true);
-  });
-  it('Renders SnapShot Component', () => {
-    const wrapper = shallow(<ReviewSnapShot />);
     expect(wrapper.length).toEqual(1);
   });
 });
